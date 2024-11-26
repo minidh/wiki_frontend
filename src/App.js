ff748167ccefdf787c860fae885178765742a2b0
minidh <m_dohui59@naver.com>
2024-11-26 20:15:35 +0900

    11_26

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Route, useNavigate } from 'react-router-dom';
+import WikiPage from './WikiPage';
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const navigate = useNavigate();
+
+  const handlekeyPress = (e) => {
+    if (e.key === 'Enter') {
+      console.log(searchQuery)
+      navigate(`/wiki/${searchQuery}`)
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='container'>
+      <Routes>
+        <Route path='/wiki/:searchQuery' element={<WikiPage></WikiPage>}>+</Route>
+      </Routes>
+      <header>
+        <h1>Restaurants Wiki</h1>
+        <div className='search-container'>
+          <div className='search-icon'>🔍</div>
+          <input 
+            type='text' 
+            className='search-input' 
+            placeholder='검색하기' 
+            onChange={(e) => setSearchQuery(e.target.value)} 
+            onKeyDown={handlekeyPress}
+          ></input>
+        </div>
       </header>
     </div>
-  );
-}
+  )
+};
 
 export default App;
---
+++ src/WikiPage.js
@@ -0,0 +1,11 @@
+import { useParams } from "react-router-dom";
+
+function WikiPage() {
+    const {param} = useParams();
+    return(
+        <div className="container">
+            <h1>위키 정보가 들어갈 곳 입니다.</h1>
+            <div>{param}</div>
+        </div>
+    )
+}
